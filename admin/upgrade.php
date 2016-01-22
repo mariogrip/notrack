00e74bebfc1fafbb2683fbeecb4b9f6944e992ef
Quidsup <ooop.btinternet.com>
2016-01-22 20:05:44 +0000

    changed upgrade notification method

--- admin/upgrade.php
+++ admin/upgrade.php
@@ -2,7 +2,8 @@
 <html>
 <head>
     <meta charset="UTF-8" />
-    <link href="./css/master.css" rel="stylesheet" type="text/css" />    
+    <link href="./css/master.css" rel="stylesheet" type="text/css" />
+    <link rel="icon" type="image/png" href="./favicon.png" />
     <title>NoTrack Upgrade</title>
 </head>
 
@@ -13,8 +14,21 @@ $CurTopMenu = 'upgrade';
 include('topmenu.html');
 echo "<h1>NoTrack Upgrade</h1>\n";
 echo  "<br />\n";
-$Version=0.3;
 
+$Version="0.4";
+$Config = array();
+
+function Load_Config_File() {
+  global $Config;
+  if (!file_exists('/etc/notrack/notrack.conf')) {
+    die("Error file /etc/notrack/notrack.conf doesn't exist");
+  }
+  $Config = parse_ini_file('/etc/notrack/notrack.conf');
+  
+  return null;
+}
+
+//Main---------------------------------------------------------------
 if (isset($_GET['u'])) {                        //Check if we are running upgrade or displaying status
   if ($_GET['u'] == '1') {                      //Doing the upgrade
     echo '<div class="splitrow"><h3>Upgrading NoTrack</h3>';
@@ -28,41 +42,40 @@ if (isset($_GET['u'])) {                        //Check if we are running upgrad
   }
 }
 else {                                           //Just displaying status
-  $ch = curl_init();                             //Initiate curl
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');
-  curl_setopt($ch, CURLOPT_URL,'https://raw.githubusercontent.com/quidsup/notrack/master/conf/notrack.conf');
-  $Data = curl_exec($ch);                        //Download version file
-  curl_close($ch);                               //Close curl
+  Load_Config_File();  
   
-  $V = floatval(trim(explode("=", $Data)[1]));   //Extract version value
-  
-  if ($V > $Version) {                           //Upgrade Needed
+  if (array_key_exists('LatestVersion', $Config)) {
+    if ($Version == $Config[LatestVersion]) {    //See if upgrade Needed
+      echo '<p>You&#39;re running the latest version v'.$Version.'</p>';
+      echo '<h3><a class="linkbutton" href="./">Back</a></h3>';      
+    }
+    else { 
+      echo '<div class="row">';
+      echo '<p>Currently running version: v'.$Version.'</p>';
+      echo '<p>Latest version available: v'.$Config[LatestVersion].'</p><br /></div>'."\n";
+      echo '<div class="splitrow"><h3>Changelog</h3>';
+      $ch = curl_init();                           //Initiate curl
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');
+      curl_setopt($ch, CURLOPT_URL,'https://raw.githubusercontent.com/quidsup/notrack/master/changelog.txt');
+      $Data = curl_exec($ch);                      //Download Changelog
+      curl_close($ch);                             //Close curl
+      echo "<pre>\n";
+      echo $Data;
+      echo "</pre></div>\n";
+      echo '<h3><a class="linkbutton" href="?u=1">Upgrade</a>&nbsp;<a class="linkbutton" href="./">Back</a></h3>';
+    }
+  }
+  else {                                       //Config line missing
     echo '<div class="row">';
-    echo '<p>Currently running version: v'.$Version.'</p>';
-    echo '<p>Latest version available: v'.$V.'</p><br /></div>'."\n";
-    echo '<div class="splitrow"><h3>Changelog</h3>';
-    $ch = curl_init();                           //Initiate curl
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');
-    curl_setopt($ch, CURLOPT_URL,'https://raw.githubusercontent.com/quidsup/notrack/master/changelog.txt');
-    $Data = curl_exec($ch);                      //Download Changelog
-    curl_close($ch);                             //Close curl
-    echo "<pre>\n";
-    echo $Data;
-    echo "</pre></div>\n";
-    echo '<h3><a class="linkbutton" href="?u=1">Upgrade</a>&nbsp;<a class="linkbutton" href="./">Back</a></h3>';
+    echo '<p>Unable to identify latest version available. <br />Fix with:</p>';
+    echo '<pre>';
+    echo "sudo notrack\n";
+    echo "</pre>\n";
+    echo "<br /></div>\n";    
   }
-  elseif ($V == $Version) {                      //Equal version
-    echo '<p>You&#39;re running the latest version v'.$Version.'</p>';
-    echo '<h3><a class="linkbutton" href="./">Back</a></h3>';
-  }
-  else {                                         //Ahead
-    echo '<p>You&#39;re ahead of the latest release v'.$V.'</p>';
-    echo '<h3><a class="linkbutton" href="./">Back</a></h3>';
-  }
+  
 }
 ?> 
 </div>
